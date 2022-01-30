3eb045a6cfdbd3f4a088195fa37f70bbfdd4950e
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2022-01-30 15:14:54 -0800

    add OICE

--- algorithms/src/LongestSubstringWithoutRepeatingCharacters.java
+++ algorithms/src/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,2 +1,10 @@
+// O.I.C.E. - An effective way to consume requirements
+// Output   - string containing the longest substring without repeats
+// Input    - string
+// Constraints and Complexity
+//      Are there any stated constraints to the problem?
+//      Are there any indicators of time vs space trade-offs?
+// Examples and Edge cases
+
 public class LongestSubstringWithoutRepeatingCharacters {
 }
