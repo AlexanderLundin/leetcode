8ef1a50f3de55c3f55680944f1bf75b7a416d08e
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2022-01-30 15:56:55 -0800

    cleanup OICE comment location

--- algorithms/src/Facebook/LongestSubstringWithoutRepeatingCharacters.java
+++ algorithms/src/Facebook/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,14 +1,15 @@
-package Facebook;// O.I.C.E. - An effective way to consume requirements
+package Facebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// O.I.C.E. - An effective way to consume requirements
 // Output   - string containing the length of the longest substring without repeats
 // Input    - string
 // Constraints and Complexity
 //      Are there any stated constraints to the problem?
 //      Are there any indicators of time vs space trade-offs?
 // Examples and Edge cases
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String args[]){
         System.out.println(find("abcabcbb"));
