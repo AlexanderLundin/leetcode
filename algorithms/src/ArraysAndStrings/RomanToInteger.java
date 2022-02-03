306840ade29fc421815090b71d401c24cd64981a
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2022-02-02 22:18:17 -0800

    solution passed

--- algorithms/src/ArraysAndStrings/RomanToInteger.java
+++ algorithms/src/ArraysAndStrings/RomanToInteger.java
@@ -8,10 +8,21 @@ package ArraysAndStrings;
 //      Are there any indicators of time vs space trade-offs?
 // Examples and Edge cases
 
-public class RomanToInteger {
+import java.util.HashMap;
 
+public class RomanToInteger {
+    static HashMap<String, Integer> values = new HashMap<String, Integer>();
+    static{
+        values.put("M", 1000);
+        values.put("D", 500);
+        values.put("C", 100);
+        values.put("L", 50);
+        values.put("X", 10);
+        values.put("V", 5);
+        values.put("I", 1);
+    }
     public static void main(String args[]){
-        assertionDriver("I", 3);
+        assertionDriver("III", 3);
         assertionDriver("LVIII", 58);
         assertionDriver("MCMXCIV", 1994);
 
@@ -26,6 +37,24 @@ public class RomanToInteger {
     }
 
     public int romanToInt(String s) {
-        return 3;
+        int sum = 0;
+
+        for (int i = 0; i < s.length();){
+            String currentSymbol = s.substring(i, i + 1);
+            int currentValue = values.get(currentSymbol);
+            int nextValue = 0;
+            if (i + 1 < s.length()){
+                String nextSymbol = s.substring(i + 1, i + 2);
+                nextValue = values.get(nextSymbol);
+            }
+            if (currentValue < nextValue){
+                sum += (nextValue - currentValue);
+                i += 2;
+            }else{
+                sum += currentValue;
+                i += 1;
+            }
+        }
+        return sum;
     }
 }
