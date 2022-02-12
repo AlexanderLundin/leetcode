acd840e9a65723b163c8b1cc2c1a45386d0ec67e
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2022-02-12 12:24:17 -0800

    add solution

--- algorithms/src/ArraysAndStrings/RemoveDuplicatesFromSortedArray.java
+++ algorithms/src/ArraysAndStrings/RemoveDuplicatesFromSortedArray.java
@@ -8,7 +8,7 @@ public class RemoveDuplicatesFromSortedArray {
     public static void main(String args[]){
 
 
-        assertionDriver(new int[]{-1,0,1,2,-1,-4}, new int[]{-1,0,1});
+        assertionDriver(new int[]{1,1,2}, new int[]{1,2});
 
     }
 
@@ -25,7 +25,15 @@ public class RemoveDuplicatesFromSortedArray {
     }
 
     public int removeDuplicates(int[] nums) {
-
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
     }
 
 }
