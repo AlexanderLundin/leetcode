78518bd3a344844e076cc1fcc7a87f80bd81c397
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2022-02-18 10:58:08 -0800

    add test

--- algorithms/test/ArraysAndStrings/NextPermutationTest.java
+++ algorithms/test/ArraysAndStrings/NextPermutationTest.java
@@ -1,5 +1,6 @@
 package ArraysAndStrings;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -8,5 +9,12 @@ public class NextPermutationTest {
 
     @Test
     public void testNextPermutation() {
+
+        NextPermutation o = new NextPermutation();
+        int[] nums = new int[]{1,2,3};
+        int[] expected = new int[]{1,3,2};
+        o.nextPermutation(nums);
+        Assert.assertEquals(nums, expected);
+
     }
 }
