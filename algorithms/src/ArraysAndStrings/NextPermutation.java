b2488b4eea2bc6ed312bdd0058a364aec4095a21
AlexanderLundin <54223212+AlexanderLundin@users.noreply.github.com>
2022-02-18 10:54:31 -0800

    implement solution

--- algorithms/src/ArraysAndStrings/NextPermutation.java
+++ algorithms/src/ArraysAndStrings/NextPermutation.java
@@ -1,4 +1,37 @@
 package ArraysAndStrings;
 
 public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]){
+            i--;
+        }
+        int j = i;
+        if (i > 0){
+            
+            while (j > nums.length && nums[j] <= nums[i]){
+                j++;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, j);
+
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int iNum = nums[i];
+        int jNum = nums[j];
+        nums[i] = jNum;
+        nums[j] = iNum;
+    }
 }
---
+++ algorithms/test/ArraysAndStrings/NextPermutationTest.java
@@ -0,0 +1,12 @@
+package ArraysAndStrings;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class NextPermutationTest {
+
+    @Test
+    public void testNextPermutation() {
+    }
+}
